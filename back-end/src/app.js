9317da47f37b8a642631e9583f3a6ba215148119
LCDS97 <lucas.cds1997@gmail.com>
2021-06-20 21:59:59 -0300

    update app js with crud base

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,8 +1,32 @@
 const express = require('express');
 
 const app = express();
-app.get('/teste', (req, res) => {
-    return res.json({message: 'Salve!'})
+app.use(express.json())
+app.use(express.urlencoded({ extended: false}))
+
+items = [];
+
+app.get('/items', (req, res) => {
+    return res.json(items);
+})
+
+app.post('/items', (req, res) => {
+    const { name } = req.body;
+    items.push(name);
+    return res.json(items);
+})
+
+app.put('/items/:index', (req, res) => {
+    const { index } = req.params;
+    const { name } = req.body;
+    items[index] = name;
+    return res.json(items);
+})
+
+app.delete('/items/:index', (req, res) => {
+    const { index } = req.params;
+    items.splice(index, 1);
+    return res.json(items);
 })
 
 module.exports = app
