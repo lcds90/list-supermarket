54d21d8df8d2db73be9f5c97720eb97acd4b9208
Leonardo Santos <lcds90@gmail.com>
2021-07-18 22:41:58 -0300

    firebase config

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
-import styles from '../styles/Home.module.css';
-import { Navbar } from '../components/Navbar';
-import { NavItem } from '../components/NavItem';
+import styles from '@styles/Home.module.css';
+import { Navbar } from '@components/Navbar';
+import { NavItem } from '@components/NavItem';
+import { database } from '@services/firebase';
 export default function Home() {
+  // const connection = database.
   return (
     <>
       <main className={styles.container}>
@@ -10,6 +12,10 @@ export default function Home() {
           <NavItem text='Item 1' color='secondary' />
           <NavItem text='Item 2' color='tertiary' />
         </Navbar>
+        <hr />
+        <div>
+          teste
+        </div>
       </main>
     </>
   );
